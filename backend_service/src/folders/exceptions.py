b5b90e74ab337354fae3021a6a38dd742c43dc68
gt005 <karimhamid@yandex.ru>
2024-03-18 20:09:04 +0300

    Добавлен fix на повторную загрузку новости в папку

--- backend_service/src/folders/exceptions.py
+++ backend_service/src/folders/exceptions.py
@@ -4,3 +4,8 @@ from src.common.exceptions import DetailedHTTPException
 class FolderWithSuchTitleAlreadyExists(DetailedHTTPException):
     STATUS_CODE = 409
     DETAIL = "Folder with such title already exists"
+
+
+class FolderAlreadyContainsNews(DetailedHTTPException):
+    STATUS_CODE = 409
+    DETAIL = "Folder already contains news"
