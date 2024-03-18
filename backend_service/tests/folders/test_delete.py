f02af41317109c8e59edff17ff14f4fd4a9b5f0a
gt005 <karimhamid@yandex.ru>
2024-03-19 00:04:15 +0300

    Fix удаления папки

--- backend_service/tests/folders/test_delete.py
+++ backend_service/tests/folders/test_delete.py
@@ -3,6 +3,7 @@ from sqlalchemy import select
 from sqlalchemy.ext.asyncio import AsyncSession
 
 from src.folders.models import FolderModel
+from src.news.models import NewsModel
 from src.users.models import UserModel
 
 
@@ -33,3 +34,21 @@ async def test_not_auth_user(client: AsyncClient, folder: FolderModel):
     response = await client.delete(f"/api/v1/folders/{folder.id}")
     assert response.status_code == 403
     assert response.json() == {"detail": "Not authenticated"}
+
+
+async def test_delete_with_news(
+    auth_client: AsyncClient,
+    db_session: AsyncSession,
+    folder: FolderModel,
+    news_in_folder: NewsModel
+):
+    response = await auth_client.delete(f"/api/v1/folders/{folder.id}")
+    assert response.status_code == 204
+
+    deleted_folder = await db_session.scalar(select(FolderModel).where(FolderModel.id == folder.id))
+    assert deleted_folder is None
+
+    not_deleted_news = await db_session.scalar(
+        select(NewsModel).where(NewsModel.id == news_in_folder.id)
+    )
+    assert not_deleted_news is not None
