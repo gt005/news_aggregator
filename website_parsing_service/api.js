f4b02d74073c7c67dafac0a19d9a7f1d4c852fa7
gt005 <karimhamid@yandex.ru>
2024-12-17 02:51:00 +0300

    Изменены ссылки для парсеров

--- website_parsing_service/api.js
+++ website_parsing_service/api.js
@@ -15,7 +15,7 @@ export const sendParsedNewsToBackendServer = async (articles) => {
 
     try {
         await axios.post(
-            "http://backend-news-data-processor.default.svc.cluster.local:8002/infra/v1/save-news/list",
+            "http://backend-news-data-processor-svc.default.svc.cluster.local:8002/infra/v1/save-news/list",
             articles,
             {
                 headers: { Authorization: `Bearer ${token}` },
